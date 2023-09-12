f79829ef658292ae086b0771722fd029ec0c98e1
Usman Musa <usmanmusa1920@gmail.com>
2023-09-12 02:53:28 +0100

    remove annoying alert, when code is copied in documentation page

--- static/index.js
+++ static/index.js
@@ -3,3 +3,10 @@ function increment() {
     counter++;
     document.getElementById("views").innerHTML = counter;
 }
+
+function rest_copy_btn_name() {
+    // function that rest previous clipboard copy button inner text
+    document.querySelectorAll('.copy-button').forEach((blck) => {
+        blck.innerText = 'Copy';
+    });
+}
